26cc4aead84672b58c1d12299e3df001f4eb8872
NightEule5 <24661563+NightEule5@users.noreply.github.com>
2024-07-21 00:20:55 -0600

    Make traits object-safe

--- src/lib.rs
+++ src/lib.rs
@@ -111,48 +111,48 @@ pub trait DataSource {
 		Ok(bytes)
 	}
 	/// Reads an array with a size of `N` bytes.
-	fn read_array<const N: usize>(&mut self) -> Result<[u8; N]> {
+	fn read_array<const N: usize>(&mut self) -> Result<[u8; N]> where Self: Sized {
 		let mut array = [0; N];
 		self.read_exact_bytes(&mut array)?;
 		Ok(array)
 	}
 
 	/// Reads a [`u8`].
-	fn read_u8(&mut self) -> Result<u8> { self.read_int() }
+	fn read_u8(&mut self) -> Result<u8> { self.read_int_be_spec() }
 	/// Reads an [`i8`].
-	fn read_i8(&mut self) -> Result<i8> { self.read_int() }
+	fn read_i8(&mut self) -> Result<i8> { self.read_int_be_spec() }
 	/// Reads a big-endian [`u16`].
-	fn read_u16(&mut self) -> Result<u16> { self.read_int() }
+	fn read_u16(&mut self) -> Result<u16> { self.read_int_be_spec() }
 	/// Reads a big-endian [`i16`].
-	fn read_i16(&mut self) -> Result<i16> { self.read_int() }
+	fn read_i16(&mut self) -> Result<i16> { self.read_int_be_spec() }
 	/// Reads a little-endian [`u16`].
-	fn read_u16_le(&mut self) -> Result<u16> { self.read_int_le() }
+	fn read_u16_le(&mut self) -> Result<u16> { self.read_int_le_spec() }
 	/// Reads a little-endian [`i16`].
-	fn read_i16_le(&mut self) -> Result<i16> { self.read_int_le() }
+	fn read_i16_le(&mut self) -> Result<i16> { self.read_int_le_spec() }
 	/// Reads a big-endian [`u32`].
-	fn read_u32(&mut self) -> Result<u32> { self.read_int() }
+	fn read_u32(&mut self) -> Result<u32> { self.read_int_be_spec() }
 	/// Reads a big-endian [`i32`].
-	fn read_i32(&mut self) -> Result<i32> { self.read_int() }
+	fn read_i32(&mut self) -> Result<i32> { self.read_int_be_spec() }
 	/// Reads a little-endian [`u32`].
-	fn read_u32_le(&mut self) -> Result<u32> { self.read_int_le() }
+	fn read_u32_le(&mut self) -> Result<u32> { self.read_int_le_spec() }
 	/// Reads a little-endian [`i32`].
-	fn read_i32_le(&mut self) -> Result<i32> { self.read_int_le() }
+	fn read_i32_le(&mut self) -> Result<i32> { self.read_int_le_spec() }
 	/// Reads a big-endian [`u64`].
-	fn read_u64(&mut self) -> Result<u64> { self.read_int() }
+	fn read_u64(&mut self) -> Result<u64> { self.read_int_be_spec() }
 	/// Reads a big-endian [`i64`].
-	fn read_i64(&mut self) -> Result<i64> { self.read_int() }
+	fn read_i64(&mut self) -> Result<i64> { self.read_int_be_spec() }
 	/// Reads a little-endian [`u64`].
-	fn read_u64_le(&mut self) -> Result<u64> { self.read_int_le() }
+	fn read_u64_le(&mut self) -> Result<u64> { self.read_int_le_spec() }
 	/// Reads a little-endian [`i64`].
-	fn read_i64_le(&mut self) -> Result<i64> { self.read_int_le() }
+	fn read_i64_le(&mut self) -> Result<i64> { self.read_int_le_spec() }
 	/// Reads a big-endian [`u128`].
-	fn read_u128(&mut self) -> Result<u128> { self.read_int() }
+	fn read_u128(&mut self) -> Result<u128> { self.read_int_be_spec() }
 	/// Reads a big-endian [`i128`].
-	fn read_i128(&mut self) -> Result<i128> { self.read_int() }
+	fn read_i128(&mut self) -> Result<i128> { self.read_int_be_spec() }
 	/// Reads a little-endian [`u128`].
-	fn read_u128_le(&mut self) -> Result<u128> { self.read_int_le() }
+	fn read_u128_le(&mut self) -> Result<u128> { self.read_int_le_spec() }
 	/// Reads a little-endian [`i128`].
-	fn read_i128_le(&mut self) -> Result<i128> { self.read_int_le() }
+	fn read_i128_le(&mut self) -> Result<i128> { self.read_int_le_spec() }
 	/// Reads a big-endian [`usize`]. To make streams consistent across platforms,
 	/// [`usize`] is fixed to the size of [`u64`] regardless of the target platform.
 	fn read_usize(&mut self) -> Result<usize> {
@@ -175,20 +175,18 @@ pub trait DataSource {
 	}
 
 	/// Reads a big-endian integer.
-	fn read_int<T: PrimInt + Pod>(&mut self) -> Result<T> {
-		self.read_data::<T>().map(T::from_be)
+	fn read_int<T: PrimInt + Pod>(&mut self) -> Result<T> where Self: Sized {
+		self.read_int_be_spec()
 	}
 	/// Reads a little-endian integer.
-	fn read_int_le<T: PrimInt + Pod>(&mut self) -> Result<T> {
-		self.read_data::<T>().map(T::from_le)
+	fn read_int_le<T: PrimInt + Pod>(&mut self) -> Result<T> where Self: Sized {
+		self.read_int_le_spec()
 	}
 
 	/// Reads a value of generic type `T` supporting an arbitrary bit pattern. See
 	/// [`Pod`].
-	fn read_data<T: Pod>(&mut self) -> Result<T> {
-		let mut value = T::zeroed();
-		self.read_exact_bytes(bytes_of_mut(&mut value))?;
-		Ok(value)
+	fn read_data<T: Pod>(&mut self) -> Result<T> where Self: Sized {
+		self.read_data_spec()
 	}
 
 	/// Reads up to `count` bytes of UTF-8 into `buf`, returning the string read.
@@ -216,6 +214,23 @@ pub trait DataSource {
 	fn read_utf8_to_end<'a>(&mut self, buf: &'a mut String) -> Result<&'a str>;
 }
 
+/// Helper extension trait for reading generic data from an unsized source.
+trait ReadSpec<T: Pod>: DataSource {
+	fn read_int_be_spec(&mut self) -> Result<T> where T: PrimInt {
+		self.read_data_spec().map(T::from_be)
+	}
+	fn read_int_le_spec(&mut self) -> Result<T> where T: PrimInt {
+		self.read_data_spec().map(T::from_le)
+	}
+	fn read_data_spec(&mut self) -> Result<T> {
+		let mut value = T::zeroed();
+		self.read_exact_bytes(bytes_of_mut(&mut value))?;
+		Ok(value)
+	}
+}
+
+impl<S: DataSource + ?Sized, T: Pod> ReadSpec<T> for S { }
+
 pub trait DataSink {
 	/// Writes all bytes from `buf`. Equivalent to [`Write::write_all`].
 	/// 
@@ -223,41 +238,41 @@ pub trait DataSink {
 	fn write_bytes(&mut self, buf: &[u8]) -> Result;
 
 	/// Writes a [`u8`].
-	fn write_u8(&mut self, value: u8) -> Result { self.write_int(value) }
+	fn write_u8(&mut self, value: u8) -> Result { self.write_int_be_spec(value) }
 	/// Writes an [`i8`].
-	fn write_i8(&mut self, value: i8) -> Result { self.write_int(value) }
+	fn write_i8(&mut self, value: i8) -> Result { self.write_int_be_spec(value) }
 	/// Writes a big-endian [`u16`].
-	fn write_u16(&mut self, value: u16) -> Result { self.write_int(value) }
+	fn write_u16(&mut self, value: u16) -> Result { self.write_int_be_spec(value) }
 	/// Writes a big-endian [`i16`].
-	fn write_i16(&mut self, value: i16) -> Result { self.write_int(value) }
+	fn write_i16(&mut self, value: i16) -> Result { self.write_int_be_spec(value) }
 	/// Writes a little-endian [`u16`].
-	fn write_u16_le(&mut self, value: u16) -> Result { self.write_int_le(value) }
+	fn write_u16_le(&mut self, value: u16) -> Result { self.write_int_le_spec(value) }
 	/// Writes a little-endian [`i16`].
-	fn write_i16_le(&mut self, value: i16) -> Result { self.write_int_le(value) }
+	fn write_i16_le(&mut self, value: i16) -> Result { self.write_int_le_spec(value) }
 	/// Writes a big-endian [`u32`].
-	fn write_u32(&mut self, value: u32) -> Result { self.write_int(value) }
+	fn write_u32(&mut self, value: u32) -> Result { self.write_int_be_spec(value) }
 	/// Writes a big-endian [`i32`].
-	fn write_i32(&mut self, value: i32) -> Result { self.write_int(value) }
+	fn write_i32(&mut self, value: i32) -> Result { self.write_int_be_spec(value) }
 	/// Writes a little-endian [`u32`].
-	fn write_u32_le(&mut self, value: u32) -> Result { self.write_int_le(value) }
+	fn write_u32_le(&mut self, value: u32) -> Result { self.write_int_le_spec(value) }
 	/// Writes a little-endian [`i32`].
-	fn write_i32_le(&mut self, value: i32) -> Result { self.write_int_le(value) }
+	fn write_i32_le(&mut self, value: i32) -> Result { self.write_int_le_spec(value) }
 	/// Writes a big-endian [`u64`].
-	fn write_u64(&mut self, value: u64) -> Result { self.write_int(value) }
+	fn write_u64(&mut self, value: u64) -> Result { self.write_int_be_spec(value) }
 	/// Writes a big-endian [`i64`].
-	fn write_i64(&mut self, value: i64) -> Result { self.write_int(value) }
+	fn write_i64(&mut self, value: i64) -> Result { self.write_int_be_spec(value) }
 	/// Writes a little-endian [`u64`].
-	fn write_u64_le(&mut self, value: u64) -> Result { self.write_int_le(value) }
+	fn write_u64_le(&mut self, value: u64) -> Result { self.write_int_le_spec(value) }
 	/// Writes a little-endian [`i64`].
-	fn write_i64_le(&mut self, value: i64) -> Result { self.write_int_le(value) }
+	fn write_i64_le(&mut self, value: i64) -> Result { self.write_int_le_spec(value) }
 	/// Writes a big-endian [`u128`].
-	fn write_u128(&mut self, value: u128) -> Result { self.write_int(value) }
+	fn write_u128(&mut self, value: u128) -> Result { self.write_int_be_spec(value) }
 	/// Writes a big-endian [`i128`].
-	fn write_i128(&mut self, value: i128) -> Result { self.write_int(value) }
+	fn write_i128(&mut self, value: i128) -> Result { self.write_int_be_spec(value) }
 	/// Writes a little-endian [`u128`].
-	fn write_u128_le(&mut self, value: u128) -> Result { self.write_int_le(value) }
+	fn write_u128_le(&mut self, value: u128) -> Result { self.write_int_le_spec(value) }
 	/// Writes a little-endian [`i128`].
-	fn write_i128_le(&mut self, value: i128) -> Result { self.write_int_le(value) }
+	fn write_i128_le(&mut self, value: i128) -> Result { self.write_int_le_spec(value) }
 	/// Writes a big-endian [`usize`]. To make streams consistent across platforms,
 	/// [`usize`] is fixed to the size of [`u64`] regardless of the target platform.
 	fn write_usize(&mut self, value: usize) -> Result {
@@ -280,22 +295,36 @@ pub trait DataSink {
 	}
 
 	/// Writes a big-endian integer.
-	fn write_int<T: PrimInt + Pod>(&mut self, value: T) -> Result {
-		self.write_data(value.to_be())
+	fn write_int<T: PrimInt + Pod>(&mut self, value: T) -> Result where Self: Sized {
+		self.write_int_be_spec(value)
 	}
 	/// Writes a little-endian integer.
-	fn write_int_le<T: PrimInt + Pod>(&mut self, value: T) -> Result {
-		self.write_data(value.to_le())
+	fn write_int_le<T: PrimInt + Pod>(&mut self, value: T) -> Result where Self: Sized {
+		self.write_int_le_spec(value)
 	}
 	/// Writes a value of an arbitrary bit pattern. See [`Pod`].
-	fn write_data<T: Pod>(&mut self, value: T) -> Result {
-		self.write_bytes(bytes_of(&value))
+	fn write_data<T: Pod>(&mut self, value: T) -> Result where Self: Sized {
+		self.write_data_spec(value)
 	}
 	/// Writes a UTF-8 string.
 	fn write_utf8(&mut self, value: &str) -> Result {
 		self.write_bytes(value.as_bytes())
 	}
 }
+
+trait WriteSpec<T: Pod>: DataSink {
+	fn write_int_be_spec(&mut self, value: T) -> Result where T: PrimInt {
+		self.write_data_spec(value.to_be())
+	}
+	fn write_int_le_spec(&mut self, value: T) -> Result where T: PrimInt {
+		self.write_data_spec(value.to_le())
+	}
+	fn write_data_spec(&mut self, value: T) -> Result {
+		self.write_bytes(bytes_of(&value))
+	}
+}
+
+impl<S: DataSink + ?Sized, T: Pod> WriteSpec<T> for S { }
 
 #[cfg(feature = "alloc")]
 unsafe fn append_utf8<R>(buf: &mut String, read: R) -> Result<&str>
